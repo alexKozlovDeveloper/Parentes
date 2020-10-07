61432f43c2674f71c9f857640ca019317d62b459
Aliaksei <AlexKozlovDeveloper@gmail.com>
2020-10-07 06:11:22 +0300

    Add new template projects

---
+++ Parentes/DarkWebsite.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using DarkWebsite.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DarkWebsite.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexViewNameEqualIndex()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.Equal("Index", result?.ViewName);
+        }
+    }
+}
---
+++ Parentes/DarkWebsite/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DarkWebsite.Models;
+
+namespace DarkWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Hello world!";
+            return View("Index");
+        }
+    }
+}
